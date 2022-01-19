58338b355ea13f0cf27cc14f4de6bb14ba4f64c5
Amulya Varshney <55329524+amulyavarshney@users.noreply.github.com>
2022-01-20 01:04:54 +0530

    Time: 628 ms (70.77%), Space: 14.8 MB (35.85%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -1,35 +1,18 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
         """
-        Find the prefix sum for each element in nums, and for each starting position in nums             find the longest subarray with maximum number of 1's using Binary Search.
-        > ans = k
-        > For each i in range [0..n-1]:
-        > Binary search to find the rightmost index j, where subarray nums[i..j] contains 
-          enough (j-i+1) ones if we flip at most k zeros.
-        > ans = max(ans, j - i + 1)
-        > The result is ans.
+        For each element in nums, try to find the longest subarray using sliding window.
+        If #0 in (nums[left] ~ nums[right]) <= K, we continue to increment j. else if #0 in             (nums[left] ~ nums[right]) > K, we increment left (as well as right).
         
-        TC = O(NlogN), SC = O(N)
+        TC = O(n), SC = O(1)
         """
-        n = len(nums)
-        ps = [0]
-        for p in nums:
-            ps.append(ps[-1]+p)
-        def binarySearch(start):
-            l, r, x = start, n-1, start-1
-            while l<=r:
-                m = l+(r-l)//2
-                z = m-start+1
-                c1 = ps[m+1]-ps[start]
-                if c1+k >= z:
-                    x = m
-                    l = m+1
-                else:
-                    r = m-1
-            return x
-        ans = k
-        for i in range(n):
-            j = binarySearch(i)
-            ans = max(ans, j-i+1)
-        return ans
-            
+        start, c, m = 0, 0, 0
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                c += 1
+            while c > k:
+                if nums[start] == 0:
+                    c -= 1
+                start += 1
+            m = max(m, i-start+1)
+        return m
