d069f14946d27d60c0ef03dc4c171899a04c1359
Amulya Varshney <55329524+amulyavarshney@users.noreply.github.com>
2022-02-18 14:32:41 +0530

    Time: 68 ms (10.31%), Space: 14.1 MB (62.58%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,11 +1,15 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
-        ans = [[]]
-        for i in range(n):
-            m = len(ans)
-            for j in range(len(ans)):
-                ans.append(ans[j]+[nums[i]])
+        ans = []
+        def rec(s, i):
+            if len(s)==k:
+                ans.append(s[:])
+                return
+            for j in range(i, n):
+                s.append(nums[j])
+                rec(s, j+1)
+                s.pop()
+        for k in range(n+1):
+            rec([], 0)
         return ans
-            
-        
