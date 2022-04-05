55c37f6ac9cebd1a8239794e9cb2c9c83ce796d2
Amulya Varshney <55329524+amulyavarshney@users.noreply.github.com>
2022-04-05 17:17:52 +0530

    Time: 425 ms (49.34%), Space: 16.3 MB (91.11%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -3,17 +3,17 @@ class Solution:
         n, m = len(grid), len(grid[0])
         d = [(0,1), (1,0), (-1,0), (0,-1)]
         vis = [[False]*m for i in range(n)]
-        def dfs(i,j):
-            vis[i][j] = True
-            for dx,dy in d:
-                x, y = i+dx, j+dy
-                if 0<=x<n and 0<=y<m and grid[x][y]=='1' and vis[x][y]==False:
-                    dfs(x,y)
-                    
         c = 0
         for i in range(n):
             for j in range(m):
                 if grid[i][j]=='1' and vis[i][j]==False:
                     c += 1
-                    dfs(i,j)
+                    stack = [(i,j)]
+                    while len(stack)>0:
+                        x, y = stack.pop()
+                        vis[x][y] = True
+                        for dx,dy in d:
+                            xx, yy = x+dx, y+dy
+                            if 0<=xx<n and 0<=yy<m and grid[xx][yy]=='1' and vis[xx][yy]==False:
+                                stack.append((xx,yy))
         return c
