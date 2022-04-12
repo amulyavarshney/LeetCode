f03201cdb9d341e771c7d94617d1c6880d58c835
Amulya Varshney <55329524+amulyavarshney@users.noreply.github.com>
2022-04-13 02:19:47 +0530

    Time: 211 ms (65.25%), Space: 14.2 MB (64.41%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.py
+++ 1260-shift-2d-grid/1260-shift-2d-grid.py
@@ -1,9 +1,19 @@
 class Solution:
     def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
         m, n = len(grid), len(grid[0])
-        new_grid = [[0]*n for _ in range(m)]
+        mini = float("inf")
+        for i in range(m):
+            for j in range(n):
+                mini = min(mini, grid[i][j])
+        for i in range(m):
+            for j in range(n):
+                grid[i][j] -= mini
         for i in range(m):
             for j in range(n):
                 r, c = divmod(i*n+j+k, n)
-                new_grid[r%m][c%n] = grid[i][j]
-        return new_grid
+                grid[r%m][c%n] = (grid[i][j]%2001)*2001 + grid[r%m][c%n]%2001
+        for i in range(m):
+            for j in range(n):
+                grid[i][j] //= 2001
+                grid[i][j] += mini
+        return grid
