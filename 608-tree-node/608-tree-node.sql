a6426a2d2ded3c81b28acfc953fc7f1e9ff4c9e2
Amulya Varshney <55329524+amulyavarshney@users.noreply.github.com>
2022-06-23 01:44:21 +0530

    Time: 346 ms (97.96%), Space: 0B (100.00%) - LeetHub

---
+++ 608-tree-node/608-tree-node.sql
@@ -0,0 +1,5 @@
+SELECT id, 'Root' AS type FROM tree WHERE p_id IS NULL
+UNION
+SELECT id, 'Leaf' AS type FROM tree WHERE id NOT IN (SELECT DISTINCT p_id FROM tree WHERE p_id IS NOT NULL) AND p_id IS NOT NULL
+UNION
+SELECT id, 'Inner' AS type FROM tree WHERE id IN (SELECT DISTINCT p_id FROM tree WHERE p_id IS NOT NULL) AND p_id IS NOT NULL;
