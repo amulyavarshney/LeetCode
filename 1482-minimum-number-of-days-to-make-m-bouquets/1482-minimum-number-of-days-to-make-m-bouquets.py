61557bfdd690319eb6d1e08c681372e7bfc031c5
Amulya Varshney <55329524+amulyavarshney@users.noreply.github.com>
2022-06-25 11:23:39 +0530

    Time: 1683 ms (67.94%), Space: 27.9 MB (12.28%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.py
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.py
@@ -2,7 +2,7 @@ class Solution:
     def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
         if(m*k > len(bloomDay)): return -1
         l, r = 1, max(bloomDay)
-        while(l<=r):
+        while(l<r):
             mid = l+(r-l)//2
             c, s = 0, 0
             for x in bloomDay:
@@ -16,7 +16,7 @@ class Solution:
             if s>=k:
                 c += s//k
             if c >= m:
-                r = mid-1
+                r = mid
             else:
                 l = mid+1
-        return r+1
+        return l
